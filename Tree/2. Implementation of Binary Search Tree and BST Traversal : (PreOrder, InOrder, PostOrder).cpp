f43131089ab8a2b31265626625ba76cd51a227ac
Sabrina Mostafa <78508655+sabrina-mostafa@users.noreply.github.com>
2021-12-18 00:08:32 -0800

    Create 2. Implementation of Binary Search Tree and BST Traversal : (PreOrder, InOrder, PostOrder).cpp

---
+++ Tree/2. Implementation of Binary Search Tree and BST Traversal : (PreOrder, InOrder, PostOrder).cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    char data ;
+    struct Node *left, *right ;
+};
+
+
+void Preorder(struct Node *root)
+{
+    if(root == NULL) return ;
+
+    printf("%c ",root->data);         // Print data
+    Preorder(root->left);                  // Visit left subtree
+    Preorder(root->right);                 // Visit right subtree
+}
+
+
+void Inorder(Node *root)
+{
+    if(root == NULL) return ;
+
+    Inorder(root->left);                 //Visit left subtree
+    printf("%c ",root->data);          //Print data
+    Inorder(root->right);                 // Visit right subtree
+}
+
+
+void Postorder(Node *root)
+{
+    if(root == NULL) return ;
+
+    Postorder(root->left) ;              // Visit left subtree
+    Postorder(root->right) ;             // Visit right subtree
+    printf("%c ",root->data) ;           // Print data
+}
+
+
+struct Node* Insert(Node *root, char data)      //  Function to Insert Node in a Binary Search Tree
+{
+    if(root == NULL)
+    {
+        root = new Node() ;
+        root->data = data ;
+        root->left = root->right = NULL ;
+    }
+
+    else if( data<=    root->data )
+        root->left = Insert(root->left, data) ;
+
+    else
+        root->right = Insert(root->right, data) ;
+
+    return root ;
+}
+
+
+
+int main()
+{
+ /*   Code To Test the logic
+       Creating an example tree
+                    M
+    		          /   \
+                 B      Q
+               /   \      \
+              A     C      Z
+*/
+
+    struct Node* root = NULL ;
+    root = Insert(root,  'M') ;
+    root = Insert(root,  'B') ;
+    root = Insert(root,  'Q') ;
+    root = Insert(root,  'Z') ;
+    root = Insert(root,  'A') ;
+    root = Insert(root,  'C') ;
+
+    cout<<"Preorder : " ;
+    Preorder(root) ;
+    cout<<"\n" ;
+
+    cout<<"Inorder : ";
+    Inorder(root) ;
+    cout<<"\n" ;
+
+    cout<<"Postorder : " ;
+    Postorder(root) ;
+    cout<<"\n" ;
+}
